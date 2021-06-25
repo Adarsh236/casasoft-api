35c3d07cd5a0e14cd535723b622095d5f87a8aef
Adarsh236 <Adarshverma11236@gmail.com>
2021-06-25 22:10:24 +0300

    debug

--- lambdas/ingredient/updateIngredientById.js
+++ lambdas/ingredient/updateIngredientById.js
@@ -11,6 +11,31 @@ exports.handler = async (event) => {
   try {
     let body = ingredientInfo(JSON.parse(event.body));
     console.log(body);
+    /* const objKeys = Object.keys(body);
+    const params = {
+      TableName: process.env.INGREDIENT_TABLE,
+      Key: marshall({ id: event.pathParameters.id }),
+      UpdateExpression: `SET ${objKeys
+        .map((_, index) => `#key${index} = :value${index}`)
+        .join(", ")}`,
+      ExpressionAttributeNames: objKeys.reduce(
+        (acc, key, index) => ({
+          ...acc,
+          [`#key${index}`]: key,
+        }),
+        {}
+      ),
+      ExpressionAttributeValues: marshall(
+        objKeys.reduce(
+          (acc, key, index) => ({
+            ...acc,
+            [`:value${index}`]: body[key],
+          }),
+          {}
+        )
+      ),
+    };
+    const body = JSON.parse(event.body); */
     const objKeys = Object.keys(body);
     const params = {
       TableName: process.env.INGREDIENT_TABLE,
